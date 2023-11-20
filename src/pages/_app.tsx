8e41889f0e980a41b2ee883a282a93b335b1670b
Tobias Lindström <tobbelindstrom93@gmail.com>
2023-11-20 10:59:10 +0100

    Only run analytics durings builds :construction:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,14 @@ import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 
 import { ProgressBar } from '../components';
+import { isBuild } from '../utilities';
 
 export const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <ProgressBar />
       <Component {...pageProps} />
-      <Analytics />
+      {isBuild() && <Analytics />}
     </>
   );
 };
