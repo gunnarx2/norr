719e756c2b0d19b4100920ac51b1a58f853ef610
Tobias Lindström <tobbelindstrom93@gmail.com>
2023-11-23 13:14:29 +0100

    Render children as is :children_crossing:

--- packages/components/src/lib/components/Portal/Portal.tsx
+++ packages/components/src/lib/components/Portal/Portal.tsx
@@ -1,16 +1,13 @@
 import {
+  Fragment,
   HTMLAttributes,
-  ReactNode,
   ReactPortal,
   RefObject,
   useMemo,
 } from 'react';
 
 import { useIsBrowser } from '@norr/hooks';
-import { clsx } from 'clsx';
 import { createPortal as ReactDOMCreatePortal } from 'react-dom';
-
-import * as classNames from './Portal.classNames';
 
 export type UsePortalReturn = {
   portalProps: {
@@ -19,9 +16,8 @@ export type UsePortalReturn = {
 };
 
 export type UsePortalProps = {
-  children: ReactNode;
   container?: RefObject<HTMLElement | null>;
-} & HTMLAttributes<HTMLDivElement>;
+} & HTMLAttributes<HTMLElement>;
 
 export const usePortal = (props: UsePortalProps): UsePortalReturn => {
   const isBrowser = useIsBrowser();
@@ -29,15 +25,12 @@ export const usePortal = (props: UsePortalProps): UsePortalReturn => {
   const portalProps = useMemo(() => {
     if (!isBrowser) return {};
 
-    const { container, className, ...restPortalProps } = props;
+    const { container, ...restPortalProps } = props;
     const containerElement = container?.current;
 
     return {
       children: ReactDOMCreatePortal(
-        <div
-          {...restPortalProps}
-          className={clsx(classNames.root, className)}
-        />,
+        <Fragment {...restPortalProps} />,
         containerElement || document.body
       ),
     };
