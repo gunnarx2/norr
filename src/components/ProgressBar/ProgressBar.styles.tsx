7b61d45350707f655c7feb9ebe9036ef4c869d42
Tobias Lindström <tobbelindstrom93@gmail.com>
2023-12-04 16:50:49 +0100

    Progress bar transitions :zap:

--- src/components/ProgressBar/ProgressBar.styles.tsx
+++ src/components/ProgressBar/ProgressBar.styles.tsx
@@ -1,11 +1,18 @@
 import css from 'styled-jsx/css';
 
 export const progressBar = css.resolve`
+  :global(#nprogress) {
+    position: fixed;
+    z-index: 1339;
+    top: 0;
+    right: 0;
+    left: 0;
+  }
+
   div {
     pointer-events: none;
     background-color: rgb(49 151 149);
-    position: fixed;
-    z-index: 1339;
+    position: absolute;
     top: 0;
     left: 0;
     width: 100%;
